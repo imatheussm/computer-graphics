5247268191a174700dca148bc339a4c92186ff69
renan <renancunhafonseca@gmail.com>
2020-12-23 13:41:19 -0300

    Add crop button

---
+++ assets/js/cg/algorithms/Crop.js
@@ -0,0 +1,64 @@
+import * as Canvas from "../elements/Canvas.js";
+import * as Instructions from "../elements/Instructions.js";
+
+import * as colors from "../constants/Colors.js";
+
+let center, radius;
+
+export function initialize() {
+    Canvas.CANVAS.off("click").on("click", centerEvent);
+    Instructions.showMessage("Choose a point to define the CENTER of the circle.");
+}
+
+function centerEvent(event) {
+    center = Canvas.getCoordinates(event);
+
+
+    Canvas.paintPixel(center, colors.BLUE, false);
+
+    Canvas.CANVAS.off("click").on("click", radiusEvent);
+    Instructions.showMessage("Choose another point to define the RADIUS of the circle.");
+}
+
+function radiusEvent(event) {
+    const border = Canvas.getCoordinates(event);
+    const x_dist = Math.pow(center[0] - border[0], 2);
+    const y_dist = Math.pow(center[1] - border[1], 2);
+    radius = parseInt(Math.sqrt(x_dist + y_dist).toString());
+
+
+    draw();
+    Canvas.refresh();
+
+    Canvas.CANVAS.off("click").on("click", centerEvent);
+    Instructions.showMessage("Choose a point to define the CENTER of the circle.");
+}
+
+function drawEight(x, y) {
+    Canvas.paintPixel([ x + center[0],  y + center[1]], colors.RED, true);
+    Canvas.paintPixel([ y + center[0],  x + center[1]], colors.RED, true);
+    Canvas.paintPixel([ y + center[0], -x + center[1]], colors.RED, true);
+    Canvas.paintPixel([ x + center[0], -y + center[1]], colors.RED, true);
+    Canvas.paintPixel([-x + center[0], -y + center[1]], colors.RED, true);
+    Canvas.paintPixel([-y + center[0], -x + center[1]], colors.RED, true);
+    Canvas.paintPixel([-y + center[0],  x + center[1]], colors.RED, true);
+    Canvas.paintPixel([-x + center[0],  y + center[1]], colors.RED, true);
+}
+
+function draw() {
+    let [x, y] = [radius, 0];
+    let error = 1 - x;
+
+    while (x >= y) {
+        drawEight(x, y);
+        y++;
+
+        if (error < 0) {
+            error += 2 * y + 1;
+        } else {
+            x--;
+            error += 2 * (y - x + 1);
+        }
+    }
+
+}
--- assets/js/cg/elements/Buttons.js
+++ assets/js/cg/elements/Buttons.js
@@ -4,6 +4,7 @@ import * as Curve from "../algorithms/Curve.js";
 import * as FloodFill from "../algorithms/FloodFill.js";
 import * as Line from "../algorithms/Line.js";
 import * as ScanLine from "../algorithms/ScanLine.js";
+import * as Crop from "../algorithms/Crop.js";
 
 export function initialize() {
     $("#circle-button").on("click", Circle.initialize);
@@ -12,5 +13,7 @@ export function initialize() {
     $("#multi-line-button").on("click", Line.initialize);
     $("#clear-button").on("click", Canvas.initialize);
     $("#scanline-button").on("click", ScanLine.initialize);
+    $("#crop-button").on("click", Crop.initialize);
+
 
 }
