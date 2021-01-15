7adb4066954b5d8a500ebfa759fc9610a93a4c4c
renan-cunha <renancunhafonseca@gmail.com>
2021-01-15 17:09:56 -0300

    Add translation button

--- assets/js/cg/elements/Buttons.js
+++ assets/js/cg/elements/Buttons.js
@@ -5,6 +5,7 @@ import * as FloodFill from "../algorithms/FloodFill.js";
 import * as Line from "../algorithms/Line.js";
 import * as ScanLine from "../algorithms/ScanLine.js";
 import * as Crop from "../algorithms/Crop.js";
+import * as Translation from "../algorithms/Translation.js";
 
 export function initialize() {
     $("#circle-button").on("click", Circle.initialize);
@@ -14,6 +15,5 @@ export function initialize() {
     $("#clear-button").on("click", Canvas.initialize);
     $("#scanline-button").on("click", ScanLine.initialize);
     $("#crop-button").on("click", Crop.initialize);
-
-
+    $("#translation-button").on("click", Translation.initialize);
 }
